b6d6ffc8d102da960ac66376b9a1a9e8a571c01c
XahedS <zahidsaleem0440@gmail.com>
2022-08-16 11:27:22 +0500

    Builded war out of project tht is working in external tomcat server

--- src/main/java/example/microserviceeurekaserver/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/example/microserviceeurekaserver/discoveryserver/DiscoveryServerApplication.java
@@ -2,14 +2,21 @@ package example.microserviceeurekaserver.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServerApplication {
+public class DiscoveryServerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DiscoveryServerApplication.class);
 	}
 
 }
